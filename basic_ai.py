8af8067fc0790d3b8e705ed85cc23785d7533e32
m-whitson <64812468+m-whitson@users.noreply.github.com>
2021-06-22 15:30:26 -0400

    creating global dicts
    not sure if this will work (mainly the zobrist hashing) but it should? maybe?

--- basic_ai.py
+++ basic_ai.py
@@ -119,26 +119,38 @@ class PacifistPlayer(ShatarAI):
         return random.choice(pacifist_moves)
 
 
+# we will hash seen boards to save space/time
+# as suggested by Prof Gold
+
+hash_to_legal_moves = {}
+hash_to_is_game_over = {}
+hash_to_best_moves = {}
+hash_to_eval = {}
+
+
+# these three dicts do not contain any information about simulation running,
+# they only store things we can get from model functions / evaluation from get_greedy_move,
+# so they can be global. Even if we have two MCTSPlayer's competing against each other,
+# neither will gain an advantage from these dicts, but both will move faster
+
+
 class MCTSPlayer(ShatarAI):
     """
     AI that will play based off of MCTS. It will keep track of win probabilities for every
     board state that it sees in dicts. To save space/time, we're going to hash boards.
     """
 
-    # we will hash seen boards to save space/time
-    # as suggested by Prof Gold
-
-    root = None
-
     def __init__(self, white):
         super().__init__(white)
+        self.root = None
 
     def get_move(self, model):
         if model.to_play is not self.white:
             raise ValueError("Trying to play on wrong turn!")
 
-        root = GameTree(model=model)
-        selected_node = root.best_child()
+        if self.root is None:
+            self.root = GameTree(model=model)
+        selected_node = self.root.best_child()
         return selected_node
 
 
@@ -149,56 +161,71 @@ def get_greedy_move(model, candidate_moves):
     :return:
     """
 
-    # if model.to_play is not self.white:
-    #    raise ValueError("Trying to play on wrong turn!")
-
-    # this is not a player class so it doesn't have self.white
-    # so I'm replacing it with model.to_play everywhere
-    # should be enforced to be the same I think?
-
-    # if there are a lot of moves with the same eval, we want to choose a random one
     best_moves_to_choose_from = []
 
-    # Set up the first one so that I can max it later
-    best_move = candidate_moves[0]
     board = model.get_board()
-    test_model = ShatarModel(board=board, to_play=model.to_play)
-    test_model.move(best_move[0], best_move[1], best_move[2], best_move[3])
-    best_move_eval = count_material_evaluation(test_model.get_board())
-    best_moves_to_choose_from.append(best_move)
+    board_hash = hash(board)
 
-    for move in candidate_moves:
-        board = model.get_board()
+    if board_hash in hash_to_best_moves:
+        best_moves_to_choose_from = hash_to_best_moves[board_hash]
+    else:
+        # if we have not evaluated this board before
+
+        # Set up the first one so that I can max it later
+        best_move = candidate_moves[0]
+
         test_model = ShatarModel(board=board, to_play=model.to_play)
-        test_model.move(move[0], move[1], move[2], move[3])
-        curr_eval = count_material_evaluation(test_model.get_board())
+        test_model.move(best_move[0], best_move[1], best_move[2], best_move[3])
 
-        if model.to_play:
-            gg = test_model.is_game_over()
-            if gg == 1:
-                return move
-            if gg == -1 or 0:
-                continue
+        test_board = test_model.get_board()
+        test_board_hash = hash(test_board)
 
-            if curr_eval == best_move_eval:
-                best_moves_to_choose_from.append(move)
-            elif curr_eval > best_move_eval:
-                best_move = move
-                best_move_eval = curr_eval
-                best_moves_to_choose_from = [move]
-        else:
-            gg = test_model.is_game_over()
-            if gg == -1:
-                return move
-            if gg == 1 or 0:
-                continue
+        best_move_eval = count_material_evaluation(test_board)
+        best_moves_to_choose_from.append(best_move)
 
-            if curr_eval == best_move_eval:
-                best_moves_to_choose_from.append(move)
-            elif curr_eval < best_move_eval:
-                best_move = move
-                best_move_eval = curr_eval
-                best_moves_to_choose_from = [move]
+        for move in candidate_moves:
+            board = model.get_board()
+            test_model = ShatarModel(board=board, to_play=model.to_play)
+            test_model.move(move[0], move[1], move[2], move[3])
+
+            test_board = test_model.get_board()
+            test_board_hash = hash(test_board)
+
+            if test_board_hash not in hash_to_eval:
+                hash_to_eval[test_board_hash] = count_material_evaluation(test_model.get_board())
+
+            curr_eval = hash_to_eval[test_board_hash]
+
+            if test_board_hash not in hash_to_is_game_over:
+                hash_to_is_game_over[test_board_hash] = test_model.is_game_over()
+            gg = hash_to_is_game_over[test_board_hash]
+
+            if model.to_play:
+
+                if gg == 1:
+                    return move
+                if gg == -1 or 0:
+                    continue
+
+                if curr_eval == best_move_eval:
+                    best_moves_to_choose_from.append(move)
+                elif curr_eval > best_move_eval:
+                    best_move = move
+                    best_move_eval = curr_eval
+                    best_moves_to_choose_from = [move]
+            else:
+
+                if gg == -1:
+                    return move
+                if gg == 1 or 0:
+                    continue
+
+                if curr_eval == best_move_eval:
+                    best_moves_to_choose_from.append(move)
+                elif curr_eval < best_move_eval:
+                    best_move = move
+                    best_move_eval = curr_eval
+                    best_moves_to_choose_from = [move]
 
     return random.choice(best_moves_to_choose_from)
 
@@ -211,6 +238,7 @@ def get_greedy_move(model, candidate_moves):
 # here's an example I don't really understand that uses some weird libraries? idk
 
 # will use transposition tables (HashMap) as a cache to store already seen board positions
+# these tables are dicts: hash_to_legal_moves, hash_to_best_moves, hash_to_is_game_over
 
 # used pseudocode on Wikipedia to write two functions below
 
@@ -277,11 +305,9 @@ class GameTree:
     A class representing a tree in Monte Carlo tree search
     """
 
-    def __init__(self, model, this_hash, parent_hash=None, parent_action=None):
-        self.parent = None
+    def __init__(self, model, parent=None, parent_action=None):
+        self.parent = parent
         self.model = model
-        self.hash = this_hash
-        self.parent_hash = parent_hash
         self.parent_action = parent_action
         # children: array of GameTrees
         self.children = []
@@ -290,19 +316,29 @@ class GameTree:
 
     # https://ai-boson.github.io/mcts/
     def untried_actions(self):
-        return self.model.generate_legal_moves()
+        board_hash = hash(self.model.board)
+
+        if board_hash not in hash_to_legal_moves:
+            hash_to_legal_moves[board_hash] = self.model.generate_legal_moves
+
+        return hash_to_legal_moves[board_hash]
 
     def expansion(self):
         # not sure if pop is the best thing here?
         # actually it probably is because you want to expand until untried_actions is empty
         action = self.untried_actions().pop()
         next_state = self.model.move(action)
-        child = GameTree(model=next_state, parent_hash=hash(self), parent_action=action)
+        child = GameTree(model=next_state, parent=self, parent_action=action)
         self.children.append(child)
         return child
 
     def is_terminal_node(self):
-        return self.model.is_game_over()
+        board_hash = hash(self.model.board)
+
+        if board_hash not in hash_to_is_game_over:
+            hash_to_is_game_over[board_hash] = self.model.is_game_over()
+
+        return hash_to_is_game_over[board_hash]
 
     # rollout
     def simulation(self):
@@ -310,8 +346,14 @@ class GameTree:
 
         # while the game is not over
         while current_state.is_game_over() != 2:
-            possible_moves = current_state.generate_legal_moves()
+
+            board_hash = hash(self.model.board)
+            if board_hash not in hash_to_legal_moves:
+                hash_to_legal_moves[board_hash] = current_state.generate_legal_moves()
+
+            possible_moves = hash_to_legal_moves[board_hash]
             action = self.rollout_policy(current_state, possible_moves)
+
             current_state = current_state.move(action)
 
         # 1 for white win, -1 for black win, 0 for draw
